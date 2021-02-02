3226c066aa03e2fd55edc1b870a67c78078a0b78
Matthew Lovell <65989756+mattblovell@users.noreply.github.com>
2021-02-02 14:05:54 -0700

    Update kodi_panel_display.py

--- kodi_panel_display.py
+++ kodi_panel_display.py
@@ -2766,12 +2766,12 @@ def main(device_handle):
                 break
             except (SystemExit, KeyboardInterrupt):
                 shutdown()
-            # except:
-            #     print(datetime.now(), "Unexpected error: ", sys.exc_info()[0])
-            #     exc_type, exc_obj, exc_tb = sys.exc_info()
-            #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            #     print(exc_type, fname, exc_tb.tb_lineno)
-            #     pass
+            except:
+                print(datetime.now(), "Unexpected error: ", sys.exc_info()[0])
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
+                pass
 
             # If connecting to Kodi over an actual network connection,
             # update times can vary.  Rather than sleeping for a fixed
